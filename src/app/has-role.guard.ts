016b37134bca56ccc40dc22f10a020f66344f4f0
nkunic <45877400+nkunic@users.noreply.github.com>
2023-08-30 16:23:07 +0200

    1 file modified

--- src/app/has-role.guard.ts
+++ src/app/has-role.guard.ts
@@ -1,12 +1,14 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { Role } from './role.enum';
 
-@Injectable()
-export class HasRoleGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
-  }
-}
+export const hasRoleGuard: CanActivateFn = (route, state) => {
+  const router: Router = inject(Router);
+  const userRole: Role = inject(AuthService).getUserRole();
+  const expectedRoles: Role[] = route.data['roles'];
+
+  const hasRole: boolean = expectedRoles.some((role) => userRole === role);
+
+  return hasRole || router.navigate(['unauthorized']);
+};
