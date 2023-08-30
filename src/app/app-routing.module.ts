9339c19f1908af882b86420fb283275c72c12025
nkunic <45877400+nkunic@users.noreply.github.com>
2023-08-30 16:24:06 +0200

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,27 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { hasRoleGuard } from './has-role.guard';
+import { HomeComponent } from './home/home.component';
+import { Role } from './role.enum';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 
-const appRoutes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   {
     path: 'admin',
     component: AdminComponent,
+    canActivate: [hasRoleGuard],
+    data: {
+      roles: [Role.ADMIN],
+    },
   },
-  {
-    path: 'unauthorized',
-    component: UnauthorizedComponent,
-  },
+  { path: 'unauthorized', component: UnauthorizedComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
